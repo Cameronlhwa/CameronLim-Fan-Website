2cacf327e77cd718efbb0f01e6f045189ad1365c
Cameron Tsang <cameronlhwa@Camerons-MacBook-Pro.local>
2025-05-06 23:45:34 -0400

    Fixed dimension issues for chat boxes

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAVJQiENUGpGEejGTyw6ATL-CwCv-YNPjU",
-  authDomain: "cameron-lim-community.firebaseapp.com",
-  projectId: "cameron-lim-community",
-  storageBucket: "cameron-lim-community.firebasestorage.app",
-  messagingSenderId: "416527574703",
-  appId: "1:416527574703:web:6a36d0ae695f48d36c565a",
-  measurementId: "G-D1DJYVEEE7"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
